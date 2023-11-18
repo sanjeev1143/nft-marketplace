be336454066417c124d1f34ec84694d38b7ce236
gauravtiwari2509 <gauravtiwari2509@gmail.com>
2023-11-18 21:54:54 +0530

    roadmap section completed

--- src/components/Roadmap/Roadmap.js
+++ src/components/Roadmap/Roadmap.js
@@ -25,41 +25,64 @@ export default function Roadmap() {
         
         </div>
             
-        <div className="mid_line">
-
-            <div className="lineCircle">
-
-                <div className="linecircleGrp1">
-                    <img src="nft-marketplace-content/smallEclipse.svg" alt="" className="circle01 circle0" />
-                    <img src="nft-marketplace-content/smallEclipse.svg" alt="" className="circle02 circle0" />
-                    
-                </div>
-
-                <div className="linecircleGrp2">
-                    <img src="nft-marketplace-content/smallEclipse.svg" alt="" className="circle03 circle0" />
-                    <img src="nft-marketplace-content/smallEclipse.svg" alt="" className="circle04 circle0" />
-                 </div>
-
-                 <div className="linecircleGrp3">
-                    <img src="nft-marketplace-content/smallEclipse.svg" alt="" className="circle05 circle0" />
-                    <img src="nft-marketplace-content/smallEclipse.svg" alt="" className="circle06 circle0" />
-                </div>
-            </div>
+      
           
-          
-            <img src="/nft-marketplace-content/Line.svg" alt="" className='line'/>
-{/* 
-           <div className="leftIndicator">
+    <div className="roadmapPattern">
+
+        <div className="borderPatternLeft">
+            <img src="/nft-marketplace-content/borderPatternRight.SVG" alt="" className='borderPatternRight' />
+            <img src="/nft-marketplace-content/borderPatternRight.SVG" alt="" className='borderPatternRight' />
+            <img src="/nft-marketplace-content/borderPatternRight.SVG" alt="" className='borderPatternRight'/>
+
+        </div>
+
+
+
+        <div className="leftIndicator">
               <img src="/nft-marketplace-content/indicatorLeft.svg" alt="" className="leftArrow" />
               <img src="/nft-marketplace-content/indicatorLeft.svg" alt="" className="leftArrow" />
               <img src="/nft-marketplace-content/indicatorLeft.svg" alt="" className="leftArrow" />   
+        </div>
+
+          <div className="mid_line">
+            
+        <div className="borderPatternRight1">
+            <img src="/nft-marketplace-content/borderPatternLeft.SVG" alt="" className='borderPatternLeft1' />
+            <img src="/nft-marketplace-content/borderPatternLeft.SVG" alt="" className='borderPatternLeft1' />
+            <img src="/nft-marketplace-content/borderPatternLeft.SVG" alt="" className='borderPatternLeft1' />
+            
+
+        </div>
+            <div className="lineCircle">
+
+            <div className="linecircleGrp1">
+                <img src="nft-marketplace-content/smallEclipse.svg" alt="" className="circle01 circle0" />
+                <img src="nft-marketplace-content/smallEclipse.svg" alt="" className="circle02 circle0" />
+        
              </div>
 
-             <div className="rightIndicator">
+            <div className="linecircleGrp2">
+                 <img src="nft-marketplace-content/smallEclipse.svg" alt="" className="circle03 circle0" />
+                 <img src="nft-marketplace-content/smallEclipse.svg" alt="" className="circle04 circle0" />
+            </div>
+
+             <div className="linecircleGrp3">
+                 <img src="nft-marketplace-content/smallEclipse.svg" alt="" className="circle05 circle0" />
+                <img src="nft-marketplace-content/smallEclipse.svg" alt="" className="circle06 circle0" />
+            </div>
+            </div>
+          <img src="/nft-marketplace-content/Line.svg" alt="" className='line'/>
+          </div>
+        
+        <div className="rightIndicator">
               <img src="/nft-marketplace-content/indicatorRight.svg" alt="" className="rightArrow" />
               <img src="/nft-marketplace-content/indicatorRight.svg" alt="" className="rightArrow" />
               <img src="/nft-marketplace-content/indicatorRight.svg" alt="" className="rightArrow" />   
-            </div> */}
+            </div> 
+        
+
+          
+  
             
         </div>
         <div className="rightCont">
